b2eb9266ee0e1192b8a5199887686368b3438423
Wes Contreras <wes@wescontreras.com>
2020-04-19 17:54:06 -0700

    Install pug, passport

--- app.js
+++ app.js
@@ -4,30 +4,39 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+// App initialization
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Passport for authentication
+var passport = require('passport');
+var passportConfig = require('./bin/auth_config');
 
+// Route handlers
+var routeIndex = require('./routes/index');
+var routeUsers = require('./routes/users');
+
+
+// Jade as view engine 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Install route handlers
+app.use('/', routeIndex);
+app.use('/users', routeUsers);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -38,4 +47,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// Export
 module.exports = app;
