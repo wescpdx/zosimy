cce4be9c963891bee25292cf623ab8f832ec6c11
Wes Contreras <wes@wescontreras.com>
2020-04-22 00:02:35 -0700

    Versioning

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const srdb = require('../bin/srdb');
+//const srdb = require('../bin/srdb');
 const log = require('../bin/logger');
 const util = require('../bin/srutils');
 
