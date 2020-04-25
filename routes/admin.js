7591e37a380ee23e277bc4f3a37804a6fbd0188a
Wes Contreras <wes@wescontreras.com>
2020-04-24 20:58:40 -0700

    Admin panel

---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const srdb = require('../bin/srdb');
+const log = require('../bin/logger');
+const util = require('../bin/srutils');
+
+// Log init
+log.setModule('admin');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('admin', { 
+    title: 'Admin Panel', 
+    version: util.appVersion
+  });
+});
+
+module.exports = router;
