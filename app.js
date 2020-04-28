9f04bc165fe06cfd46e6e015c08d01aa1a4c1c29
Wes Contreras <wes@wescontreras.com>
2020-04-28 12:44:41 -0700

    Login fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const log = require('../bin/logger');
 
 // App initialization
 const  app = express();
