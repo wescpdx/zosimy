a54aec448713d9b9c3f83e2d52d19f61b4e01904
Wes Contreras <wes@wescontreras.com>
2020-04-28 11:41:48 -0700

    Privacy policy

--- routes/privacy.js
+++ routes/privacy.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const log = require('../bin/logger');
+const util = require('../bin/srutils');
 
 /* GET page. */
 router.get('/', function(req, res, next) {
