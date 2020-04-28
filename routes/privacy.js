453a1ffbfc47b83fdb66a2b15ab2de6e6b42901f
Wes Contreras <wes@wescontreras.com>
2020-04-28 11:40:43 -0700

    Privacy policy

--- routes/privacy.js
+++ routes/privacy.js
@@ -5,9 +5,7 @@ const log = require('../bin/logger');
 /* GET page. */
 router.get('/', function(req, res, next) {
   log.setFunction('GET /');
-  var announcement = srdb.fetchAnnounce(null).then(function(rows){
-    res.render('privacy', { title: 'Seven Roses', version: util.appVersion });
-  });
+  res.render('privacy', { title: 'Seven Roses', version: util.appVersion });
 });
 
 module.exports = router;
