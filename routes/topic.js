4d9c136d9e8c0b543c0cc3826f94ced5496afe0f
Wes Contreras <wes@wescontreras.com>
2020-05-08 15:49:43 -0700

    Login button

--- routes/topic.js
+++ routes/topic.js
@@ -19,7 +19,8 @@ router.post('/edit/updatearticle/:id', function(req,res) {
     log.logVerbose('topic.edit.post: req.body = ' + JSON.stringify(req.body), 10);
     srdb.updateArticle(req.params.id, req.body.content).then(function(data) {
       res.render('edit_confirm', {
-        title: data ? 'Article Successfully Updated' : 'Article Update Failed'
+        title: data ? 'Article Successfully Updated' : 'Article Update Failed',
+        auth: true
       });
     });
   } else {
@@ -32,7 +33,8 @@ router.get('/edit/:id', function(req, res) {
   if (req.user.admin) {
     srdb.fetchArticle(req.params.id).then(function(article) {
       res.render('edit_article', {
-        title: article.title,
+        title: 'Edit: ' + article.title,
+        auth: true,
         topic: article.title,
         id: article.article_id,
         acontent: article.content,
@@ -49,6 +51,8 @@ router.get('/:id', function(req, res) {
   srdb.fetchTopic(req.params.id, req.user).then(function(content_array) {
     log.logVerbose('topic.get: content_array = ' + JSON.stringify(content_array));
     res.render(req.user.admin ? 'topicadmin' : 'topic', {
+      title: 'Topic: ' + content_array[0].display_name,
+      auth: true,
       display_name: content_array[0].display_name,
       content_array: content_array,
       charName: req.user.charname
