cfb59305c2b2df6407a0ccf030f4c14033a64097
Wes Contreras <wes@wescontreras.com>
2020-05-20 15:56:45 -0700

    Remove user editing

--- routes/profile.js
+++ routes/profile.js
@@ -19,26 +19,5 @@ router.get('/', function(req, res) {
   });
 });
 
-router.get('/edit/:id', function(req, res) {
-  log.logVerbose('profile: Entering profile for id '+req.params.id);
-  if (req.user.admin) {
-    srdb.fetchUserByID(req.params.id).then(function(user) {
-      res.render('edit_user', {
-        title: 'Edit: ' + user.player_name,
-        auth: true,
-        player_name: user.player_name,
-        char_name: user.char_name,
-        id: user.user_id,
-        email: user.email,
-        active: user.active,
-        admin: user.admin,
-        keywords: user.keywords
-      });
-    });
-  } else {
-    res.render('accessdenied');
-  }
-});
-
 
 module.exports = router;
